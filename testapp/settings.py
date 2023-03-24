b397b9e6778ae8ac207c0fb9ac0edec9d7c48931
Denis Denis <slyusarenkodenisss@gmail.com>
2023-03-24 19:49:27 +0200

    commit docker, docker-compose

--- testapp/settings.py
+++ testapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fdck+0q+ed-@o-34k-awvtm1ae5zwxb8+-xwd8ng^&b^xf%#-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0',]
 
 
 # Application definition
