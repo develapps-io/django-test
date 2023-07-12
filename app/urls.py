ea7969ae4b3e25425b7cda31e88981cfba455d8b
Denis Denis <slyusarenkodenisss@gmail.com>
2023-07-12 18:45:32 +0300

    [POD-23]: add registration and auth to the project

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
+from .views import *
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name="index"),
+    path('register/', views.register,  name='register'),
+    path('login/', views.login, name='login'),
 ]
--- testapp/settings.py
+++ testapp/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 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
+# RGVuaXNsdm06Z2hwX3dIV0R1Y09QSHBrVHkwVkYxd2ZFb3Z3NTMycWVZVDJ4VmxkcA==
