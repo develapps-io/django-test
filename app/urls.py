1279955adf2124cd4ea1aa31cc1fbb722e65a76c
Denis Denis <slyusarenkodenisss@gmail.com>
2023-07-12 18:55:45 +0300

    [POD-23] changed urls.py to pass it in lint

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
-
-from .views import *
 from . import views
+from django.urls import path
 
 
 urlpatterns = [
     path('index/', views.index, name="index"),
     path('register/', views.register,  name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='login')
 ]
